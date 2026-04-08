0d4c76e3aee22b5ae6d6a6c77532bc0a23968cab
alex <ovhfmn@gmail.com>
2026-04-08 13:20:05 +0200

    Initial commit

---
+++ src/main/scala/PrintableInstances/Cat.scala
@@ -0,0 +1,5 @@
+package PrintableInstances
+
+final case class Cat(name: String, age: Int, colour: String)
+
+
---
+++ src/main/scala/PrintableInstances/PrintableInstances.scala
@@ -0,0 +1,22 @@
+package PrintableInstances
+
+object PrintableInstances {
+  implicit val stringPrintable: Printable[String] = new Printable[String] {
+    override def format(input: String): String = input
+  }
+
+  implicit val intPrintable: Printable[Int] = new Printable[Int] {
+    override def format(input: Int): String = input.toString
+  }
+
+  implicit val catPrintable: Printable[Cat] = new Printable[Cat] {
+    override def format(cat: Cat): String = {
+      val name = Printable.format(cat.name)
+      val age = Printable.format(cat.age)
+      val colour = Printable.format(cat.colour)
+
+      s"$name is a $age y.o. $colour cat."
+    }
+  }
+  
+}
